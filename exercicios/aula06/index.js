95f797a0676ad17c4cce6776a878a9efd012aa90
jss52-source <jss52@aluno.ifnmg.edu.br>
2026-04-10 02:06:02 +0000

    erro

--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -7,12 +7,14 @@
 
 // Exercício 1 — criarSaudacao
 export function criarSaudacao(nome) {
-  // escreva seu código aqui
+  return "Ola jean, " + nome + "!";
 }
+
+
 
 // Exercício 2 — filtrarAprovados
 export function filtrarAprovados(alunos) {
-  // escreva seu código aqui
+  return alunos.filter(aluno => aluno.nota >= 60);
 }
 
 // Exercício 3 — extrairNomes
